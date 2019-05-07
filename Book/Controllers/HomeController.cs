ecf8cf5cc3407a31d0752c09ef1513ffc2e63cae
彥維 王 <oaoxd0314@gmail.com>
2019-05-07 23:46:15 +0800

    加入專案檔案。

---
+++ Book/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Book.Models;
+namespace Book.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataBaseEntities db = new DataBaseEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var HBookData = db.BookData.OrderBy(m => m.BOOK_ID).ToList();
+            return View(HBookData);
+        }
+
+        public ActionResult Creat()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Creat(BookData CBookData)
+        {
+            db.BookData.Add(CBookData);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int bookid)
+        {
+            var DBookDatas = db.BookData.Where(m => m.BOOK_ID == bookid).FirstOrDefault();
+            db.BookData.Remove(DBookDatas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int bookid)
+        {
+            var EBookDatas = db.BookData.Where(m => m.BOOK_ID == bookid).FirstOrDefault();
+            return View(EBookDatas);
+        }
+
+        public ActionResult Edit(BookData eBookData)
+        {
+            DateTime thisDay = DateTime.Today;
+            int bookid = eBookData.BOOK_ID;
+            var Datas = db.BookData.Where(m => m.BOOK_ID == bookid).FirstOrDefault();
+
+            Datas.BOOK_NAME = eBookData.BOOK_NAME;
+            Datas.BOOK_AUTHOR = eBookData.BOOK_AUTHOR;
+            Datas.BOOK_PUBLISHER = eBookData.BOOK_PUBLISHER;
+            Datas.BOOK_NOTE = eBookData.BOOK_NOTE;
+            Datas.BOOK_BOUGHT_DATE = eBookData.BOOK_BOUGHT_DATE;
+            Datas.BOOK_CLASS_ID = eBookData.BOOK_CLASS_ID;
+            Datas.BOOK_STATUS = eBookData.BOOK_STATUS;
+            Datas.BOOK_KEEPER = eBookData.BOOK_KEEPER;
+            Datas.MODIFY_DATE = thisDay;
+            Datas.MODIFY_USER = eBookData.MODIFY_USER;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult search(string searching)
+        {
+            //show Book_data 內的資料 遞減排序
+            var book = db.BookData.Where(x => x.BOOK_NAME.Contains(searching) || searching == null).ToList();
+            return View(book);
+        }
+
+    }
+}
